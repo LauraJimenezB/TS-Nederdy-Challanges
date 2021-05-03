8a825567ca7b8e2e7bb0bffdfd38ed0a54090be6
LauraJimenezB <laura271260@gmail.com>
2021-05-02 23:16:34 -0500

    challenge complete

--- src/app.ts
+++ src/app.ts
@@ -13,15 +13,86 @@ interface TemperatureSummary {
   average: number
 }
 
-function processReadings(readings: TemperatureReading[]): void {
+const allReadings = [
+	{
+		time: new Date('1/1/2021'),
+		temperature: 10,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 9,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 11,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 3,
+		city: 'New York',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 2,
+		city: 'New York',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 7,
+		city: 'New York',
+	},
+];
+
+
+let processedReading: TemperatureReading[];
+
+export function processReadings(readings: TemperatureReading[]): void {
   // add here your code
-}
-function getTemperatureSummary(
-  date: Date,
-  city: string,
-): void {
-  //add here your code
+  processedReading = readings;
+  console.log(processedReading)
 }
 
-exports.processReadings = processReadings
-exports.getTemperatureSummary = getTemperatureSummary
+export function getTemperatureSummary(date: Date, city: string): void {
+  //add here your code
+  const readingsOfTheDay: TemperatureReading[] =  
+  allReadings.filter(reading=>reading.city===city && reading.time.getTime()===date.getTime());
+  
+  class Summary implements TemperatureSummary {
+    constructor (public first:number, public last: number, public high:number, public low:number, public average:number) {
+    }
+  };
+
+  let summary;
+
+  if (readingsOfTheDay.length > 0) {
+
+    const listByTime= readingsOfTheDay.sort((a, b) => (a.time > b.time) ? 1 : -1)
+    const listByTemperature= readingsOfTheDay.sort((a, b) => (a.temperature > b.temperature) ? 1 : -1);
+
+    const first = listByTime[0].temperature;
+    const last = listByTime[listByTime.length-1].temperature;
+    const low = listByTemperature[0].temperature;
+    const high = listByTemperature[listByTemperature.length-1].temperature;
+
+    let result = 0;
+    for (let i of readingsOfTheDay) {
+      if (i.temperature) {
+        result += i.temperature;
+      }
+    }
+    const average = result / readingsOfTheDay.length;
+    
+    summary = new Summary(first, last, high, low, average);
+
+  } else {
+    summary = null;
+  } 
+  
+  console.log(summary); 
+}
+
+/* exports.processReadings = processReadings
+exports.getTemperatureSummary = getTemperatureSummary */
--- test/app.test.js
+++ test/app.test.js
@@ -1,51 +1,52 @@
+/* eslint-disable linebreak-style */
 const {
-  processReadings,
-  getTemperatureSummary,
-} = require("../dist/app")
+	processReadings,
+	getTemperatureSummary,
+} = require('../src/app.ts');
 
-const dateTest = new Date("1/1/2021")
+const dateTest = new Date('1/1/2021');
 
 const example = [
-  {
-    time: new Date("1/1/2021"),
-    temperature: 10,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 9,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 11,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 3,
-    city: "New York",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 2,
-    city: "New York",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 7,
-    city: "New York",
-  },
-]
+	{
+		time: new Date('1/1/2021'),
+		temperature: 10,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 9,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 11,
+		city: 'Utah',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 3,
+		city: 'New York',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 2,
+		city: 'New York',
+	},
+	{
+		time: new Date('1/1/2021'),
+		temperature: 7,
+		city: 'New York',
+	},
+];
 
-describe("Process information", () => {
-  test("run function with the values", () => {
-    expect(processReadings(example))
-  })
-})
+describe('Process information', () => {
+	test('run function with the values', () => {
+		expect(processReadings(example));
+	});
+});
 
-describe("Temperature of the city", () => {
-  test("run function getTemperatureSummary", () => {
-    expect(getTemperatureSummary(dateTest, "Utah")).toMatchSnapshot()
-  })
-})
+describe('Temperature of the city', () => {
+	test('run function getTemperatureSummary', () => {
+		expect(getTemperatureSummary(dateTest, 'Utah')).toMatchSnapshot();
+	});
+});
